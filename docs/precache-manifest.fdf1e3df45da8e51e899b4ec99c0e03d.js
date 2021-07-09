0d4919f720b4aa7ad77d04957f919be5ea51390b
Julian Rincon <keideriz1x@gmail.com>
2021-07-09 17:22:32 -0500

    Hola mundo

---
+++ docs/precache-manifest.fdf1e3df45da8e51e899b4ec99c0e03d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe19acc2945cd704078290d6f98968e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02bebcca80f9a5ac72d1",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bebcca80f9a5ac72d1",
+    "url": "/static/js/main.68e66e42.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
