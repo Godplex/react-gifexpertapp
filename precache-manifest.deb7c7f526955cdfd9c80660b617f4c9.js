2ae27699aaf035dbb63c88f3a340809b3673885e
Julian Rincon <keideriz1x@gmail.com>
2021-07-09 17:51:17 -0500

    Updates

---
+++ precache-manifest.deb7c7f526955cdfd9c80660b617f4c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f24c1936ea57d6049629b48a6f7e36a",
+    "url": "/react-gifexpertapp/index.html"
+  },
+  {
+    "revision": "05074452c63cf66372a9",
+    "url": "/react-gifexpertapp/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/react-gifexpertapp/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gifexpertapp/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05074452c63cf66372a9",
+    "url": "/react-gifexpertapp/static/js/main.28e05183.chunk.js"
+  },
+  {
+    "revision": "458b80fade44981dc42b",
+    "url": "/react-gifexpertapp/static/js/runtime-main.32f2a6a4.js"
+  }
+]);
